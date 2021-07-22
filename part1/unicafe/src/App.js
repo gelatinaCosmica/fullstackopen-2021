bff34f1a80a881ed0dabe9ae41cd108ffb15bbf7
gelatina <santiagozapatabohorquez@hotmail.com>
2021-07-21 21:08:29 -0500

    created feedback system including statistics: good, neutral and bad counters. Added total amount of votes, average and positive percent

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,23 +1,62 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = good * 1 + neutral * 0 + bad * -1;
+  const positive = `${(good / total) * 100}%`;
+
+  if (good > 0 || neutral > 0 || bad > 0) {
+    return (
+      <div>
+        <h2>statistics</h2>
+        <table>
+          <tbody>
+            <Statistic name="good" value={good} />
+            <Statistic name="neutral" value={neutral} />
+            <Statistic name="bad" value={bad} />
+            <Statistic name="all" value={total} />
+            <Statistic name="average" value={average} />
+            <Statistic name="positive" value={positive} />
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  return <p>No feedback given</p>;
+};
+
+const Statistic = ({ name, value }) => {
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Button = ({ handleClick, name }) => (
+  <button onClick={handleClick}>{name}</button>
+);
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodVote = () => setGood(good + 1);
+  const handleNeutralVote = () => setNeutral(neutral + 1);
+  const handleBadVote = () => setBad(bad + 1);
 
   return (
-    <div>
+    <>
       <h1>give feedback</h1>
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
-      <h1>statistics</h1>
-      <p>good 6</p>
-      <p>neutral 2</p>
-      <p>bad 1</p>
-    </div>
-  )
-}
+      <Button handleClick={handleGoodVote} name="good" />
+      <Button handleClick={handleNeutralVote} name="neutral" />
+      <Button handleClick={handleBadVote} name="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </>
+  );
+};
 
-export default App
+export default App;
