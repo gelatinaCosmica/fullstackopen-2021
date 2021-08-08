19b44c3c9ce571d884d0fcf9afb7d43f3b9ea145
gelatina <santiagozapatabohorquez@hotmail.com>
2021-08-08 01:33:50 -0500

    added heroku config

--- part3/Phonebook/phonebook-backend/index.js
+++ part3/Phonebook/phonebook-backend/index.js
@@ -103,7 +103,8 @@ app.use((request, response) => {
 })
 
 // Port
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
